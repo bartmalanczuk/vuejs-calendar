1636597aa84e529defb3dfcbec2ef7a74d49114d
Bart <bart.malanczuk@gmail.com>
2017-07-16 20:13:06 +0200

    Install moment library

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import moment from 'moment-timezone';
+import { extendMoment } from 'moment-range';
 
 import './style.scss';
 import App from './components/App.vue';
+
+extendMoment(moment);
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype, '$moment', { get() { return moment } });
 
 new Vue({
   el: '#app',
