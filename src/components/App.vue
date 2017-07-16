d79439bfc183953c3ca86082a3128329b8e74b63
Bart <bart.malanczuk@gmail.com>
2017-07-16 23:29:43 +0200

    Compute weeks and days within month

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>{{ message }}</div>
+  <div>
+    <div v-for="week in weeks">
+      <div v-for="day in week">{{ day }}</div>
+    </div>
+  </div>
 </template>
 <script>
   export default {
     data() {
       return {
-        message: 'Hello World',
+        month: 2,
+        year: 2017,
       };
+    },
+    computed: {
+      weeks() {
+        const day = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D');
+
+        return Array.from(this.$moment.range(
+          this.$moment(day).startOf('isoweek'),
+          this.$moment(day).endOf('month').endOf('isoweek'),
+        ).by('week')).map((week) => {
+          return Array.from(this.$moment.range(
+            this.$moment(week).startOf('isoweek'),
+            this.$moment(week).endOf('isoweek'),
+          ).by('day'));
+        });
+      },
     },
   };
 </script>
